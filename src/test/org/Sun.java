4137d66df7562fb6af466b9d20cb6f9f4a7bea26
ElamparithiK <elamparithikumaresan99@gmail.com>
2022-09-07 07:44:43 +0530

    sample commit

---
+++ src/test/org/Sun.java
@@ -0,0 +1,25 @@
+package test.org;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sun {
+	public static void main(String[] args) {
+		
+	
+	int l[]=new int[] {1,2,2,3,4,5,6,7,8,9,10};
+	   
+	int sum=0;
+	for (int i = 0; i < l.length; i++) {
+		sum=sum+l[i];
+		
+		
+		
+	}
+	System.out.println("s value:"+sum);
+	
+	int aveg=sum/10;
+	System.out.println(aveg);
+	}
+
+}
